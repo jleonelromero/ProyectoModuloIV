abc7ab166c795113f06ca93bd6e56598feab18f1
leonel <joseleonelmijail@gmail.com>
2023-12-17 20:16:02 -0500

    login mejorado

--- login.php
+++ login.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="css/estilo.css">
+    <!-- <link rel="stylesheet" href="css/estilo.css"> -->
+    <link rel="stylesheet" href="css/login.css">
 </head>
 
 <body>
@@ -16,18 +17,22 @@
     <form action="p_login.php" method="post">
         <fieldset id="login">
             <center>
-                <label for="txtusuario">Usuario:</label>
+                <a href="login.php">
+                <img src="img/login.jpeg"  class="img" alt="">
+
+                </a>
+                <label for="txtusuario" class="labels">Usuario</label>
                 <br>
-                <input type="text" id="txtusuario" name="txtusuario" class="txt">
+                <input type="text" id="txtusuario" name="txtusuario" class="input">
                 <br>
-                <label for="txtpassword">Contraseña:</label>
+                <label for="txtpassword" class="labels">Contraseña</label>
                 <br>
-                <input type="password" id="txtpassword" name="txtpassword" class="pwd">
+                <input type="password" id="txtpassword" name="txtpassword" class="input">
                 <br>
                 <input type="submit" value="Iniciar Sesión" class="btn">
                 <br>
                 <br>
-                <a href="registro.php">¿No tienes una cuenta? ¡Regístrate!</a>
+                <a class="hiper" href="registro.php">¿No tienes una cuenta? ¡Regístrate!</a>
             </center>
         </fieldset>
     </form>
