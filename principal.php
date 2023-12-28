f7c7be30111be6fd659acd0674ec633fe8c0fcd4
leonel <joseleonelmijail@gmail.com>
2023-12-28 00:44:58 -0500

    se intento xd

--- principal.php
+++ principal.php
@@ -10,9 +10,11 @@ $fPers = mysqli_query($cn, $sqlPersona);
 $rPers = mysqli_fetch_assoc($fPers);
 if ($rInst) {
     $tabla = "InstitucionEducativa";
+    
 } elseif ($rPers) {
     $tabla = "PersonaIndividual";
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -28,7 +30,16 @@ if ($rInst) {
 
 <body>
     <br>
+<?php
 
+if (isset($rInst['estado']) && $rInst['estado'] == 0 || isset($rPers['estado']) && $rPers['estado'] == 0) {
+    // Código cuando el estado es 0
+    echo "llenardatos";
+} else {
+    echo "hola";
+}
+
+?>
     <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
 </body>
 <?php
